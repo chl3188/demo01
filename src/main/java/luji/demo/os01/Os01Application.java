713bdc67a08954d982b57d458e0e2f6287039016
unknown <chl3188@gmail.com>
2023-07-01 00:03:14 +0900

    init: Backend 프로젝트 생성

---
+++ src/main/java/luji/demo/os01/Os01Application.java
@@ -0,0 +1,13 @@
+package luji.demo.os01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Os01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Os01Application.class, args);
+	}
+
+}
---
+++ src/test/java/luji/demo/os01/Os01ApplicationTests.java
@@ -0,0 +1,13 @@
+package luji.demo.os01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Os01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
